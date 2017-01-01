11ad2278d8a6adbb5b5ced49e0dc3a53617e4e41
loufoque <kalyan.krishnamani@hotmail.com>
2016-12-31 23:50:29 -0800

    Union find algorithm - QuickFind and QuickUnion versions

---
+++ algorithms_1/week_1/QuickFind.java
@@ -0,0 +1,64 @@
+// Algorithms - week 1
+// Quick Find Algorithm
+
+import java.util.*;
+
+public class QuickFind 
+{
+	private int[] id;
+	
+	// constructor
+	public QuickFind(int N)
+	{
+		id = new int[N];
+		for (int i = 0; i < N; i++) {
+			id[i] = i;
+		}
+	}
+	
+	// find/connected query
+	public boolean connected(int p, int q)
+	{
+		return (id[p] == id[q]);
+	}
+	
+	// union operation
+	public void union(int p, int q) 
+	{
+		// since the index of the array is the same as the content
+		// through construction (constructor), id[x] will give the 
+		// index of the element x
+		int pid = id[p]; // get the id of p in the array
+		int qid = id[q]; // get the id of q in the array
+		
+		// traverse the array and replace the id of p with id of q
+		for (int i = 0; i < id.length; i++) {
+			if (id[i] == pid)
+				id[i] = qid;
+		}
+	}
+
+	// Client code
+	public static void main(String[] args) 
+	{
+		// read from standard input
+		Scanner scan = new Scanner(System.in);
+		
+		// the first line of the input text file 
+		// indicates the number of objects.
+		// Scan it and used it in our constructor
+		int N = scan.nextInt();
+		
+		QuickFind QF = new QuickFind(N);		
+		while (scan.hasNextLine()) {
+			int p = scan.nextInt();
+			int q = scan.nextInt();
+			if (!QF.connected(p, q)) {
+				QF.union(p, q);
+				System.out.format("(%d %d)%n", p, q);
+			}
+		}		
+		scan.close();
+	}
+}
+
---
+++ algorithms_1/week_1/QuickUnion.java
@@ -0,0 +1,59 @@
+// Algorithms - week 1
+// Quick Find Algorithm
+
+import java.util.*;
+
+public class QuickUnion
+{
+	private int[] id;
+	
+	// constructor
+	public QuickUnion(int N)
+	{
+		id = new int[N];
+		for (int i = 0; i < N; i++) {
+			id[i] = i;
+		}
+	}
+	
+	// root of a node 
+	private int root(int n)
+	{
+		while(n != id[n])
+			n = id[n];
+		return n;
+	}
+	
+	// find/connected query
+	public boolean connected(int p, int q) 
+	{
+		return (root(p) == root(q));
+	}
+	
+	// union operation
+	public void union(int p, int q)
+	{
+		int proot = root(p);
+		int qroot = root(q);
+		id[proot] = qroot;
+	}
+
+	// Client code
+	public static void main(String[] args)
+	{
+		// read from standard input
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		QuickUnion QU = new QuickUnion(N);
+		while (scan.hasNextLine()) {
+			int p = scan.nextInt();
+			int q = scan.nextInt();
+			if (!QU.connected(p, q)) {
+				QU.union(p, q);
+				System.out.format("(%d %d)%n", p, q);
+			}
+		}
+		scan.close();
+	}
+
+}
